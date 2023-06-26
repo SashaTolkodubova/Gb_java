fc10c87708364aa3906fa46c5c3e1611397d41a8
SashaTolkodubova <atolkodubova@gmail.com>
2023-06-27 00:39:25 +0200

    add code lesson5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<Integer, String> phoneBook = new HashMap<>();
+        phoneBook.put(1111, "John Doe ");
+        phoneBook.put(2222, "Jane Doe ");
+        phoneBook.put(3333, "John Doe ");
+        phoneBook.put(4444, "John Doe ");
+        phoneBook.put(5555, "Jane Doe ");
+        phoneBook.put(6666, "John Doe ");
+        phoneBook.put(7777, "Jane Doe ");
+        phoneBook.put(8888, "Baby Doe");
+
+        Lesson5.printPhoneBook(Lesson5.getPhoneBook(phoneBook));
     }
 }
